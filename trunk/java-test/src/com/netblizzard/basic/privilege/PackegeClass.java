915f64cb0aadd0449f12a035e2f89af50711c39c
yongyouziyoutoo@sohu.com <yongyouziyoutoo@sohu.com@1ff9615c-348a-42f2-f0fe-bb71d646334e>
2012-02-12 02:23:32 +0000

    lsj

--- trunk/java-test/src/com/netblizzard/basic/privilege/PackegeClass.java
+++ trunk/java-test/src/com/netblizzard/basic/privilege/PackegeClass.java
@@ -2,6 +2,7 @@ package com.netblizzard.basic.privilege;
 
 public class PackegeClass {
 	public static void main(String[] arges) {
+		// protected 具有包访问权限
 		System.out.println(BaseClass.protectedInt);
 	}
 }
