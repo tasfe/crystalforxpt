247fd98627bfe5a11f8c0befe139cfec0dd33f27
yongyouziyoutoo@sohu.com <yongyouziyoutoo@sohu.com@1ff9615c-348a-42f2-f0fe-bb71d646334e>
2012-02-12 02:23:00 +0000

    lsj

---
+++ java-test/src/com/netblizzard/basic/privilege02/OtherClass.java
@@ -0,0 +1,9 @@
+package com.netblizzard.basic.privilege02;
+
+import com.netblizzard.basic.privilege.BaseClass;
+
+public class OtherClass {
+	public static void main(String[] args) {
+//		System.out.println(BaseClass.protectedInt);
+	}
+}
